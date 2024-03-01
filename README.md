bb2a93fb2f9090bf56e542425d72d6b52eb9d996
Prashant Sinha <prashantsinha94@gmail.com>
2024-03-02 00:58:30 +0530

    tox and gh workflow

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # convict_quandary
+
+![Coverage](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/prashant94/47b3dd14f7c852987394f631ada1ae1b/raw/covbadge.json
+)
--- src/convictquandary/player_logics/logic_always_cooperate_believe_truth.py
+++ src/convictquandary/player_logics/logic_always_cooperate_believe_truth.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from ..constants import Action, Belief, Persuasion
 from ..player_logic import PlayerLogic
 
@@ -8,30 +6,30 @@ class LogicAlwaysCooperateBelieveTruth(PlayerLogic):
 
     def get_persuasion(
         self,
-        player_actions: List[Action],
-        player_persuasions: List[Persuasion],
-        player_beliefs: List[Belief],
-        opponent_actions: List[Action],
-        opponent_persuasions: List[Persuasion],
+        player_actions: list[Action],
+        player_persuasions: list[Persuasion],
+        player_beliefs: list[Belief],
+        opponent_actions: list[Action],
+        opponent_persuasions: list[Persuasion],
     ) -> Persuasion:
         return Persuasion.TRUTH
 
     def get_belief(
         self,
-        player_actions: List[Action],
-        player_persuasions: List[Persuasion],
-        player_beliefs: List[Belief],
-        opponent_actions: List[Action],
-        opponent_persuasions: List[Persuasion],
+        player_actions: list[Action],
+        player_persuasions: list[Persuasion],
+        player_beliefs: list[Belief],
+        opponent_actions: list[Action],
+        opponent_persuasions: list[Persuasion],
     ) -> Belief:
         return Belief.BELIEVE
 
     def get_action(
         self,
-        player_actions: List[Action],
-        player_persuasions: List[Persuasion],
-        player_beliefs: List[Belief],
-        opponent_actions: List[Action],
-        opponent_persuasions: List[Persuasion],
+        player_actions: list[Action],
+        player_persuasions: list[Persuasion],
+        player_beliefs: list[Belief],
+        opponent_actions: list[Action],
+        opponent_persuasions: list[Persuasion],
     ) -> Action:
         return Action.COOPERATE
