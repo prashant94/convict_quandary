d4c2d567d3039aa91e60f87d5b4b94c030400ce9
Prashant Sinha <prashantsinha94@gmail.com>
2024-04-02 21:26:22 +0530

    player exception handling + changelog management

--- src/convictquandary/player_logics/logic_always_cooperate_believe_truth.py
+++ src/convictquandary/player_logics/logic_always_cooperate_believe_truth.py
@@ -4,26 +4,6 @@ from ..player_logic import PlayerLogic
 
 class LogicAlwaysCooperateBelieveTruth(PlayerLogic):
 
-    def get_persuasion(
-        self,
-        player_actions: list[Action],
-        player_persuasions: list[Persuasion],
-        player_beliefs: list[Belief],
-        opponent_actions: list[Action],
-        opponent_persuasions: list[Persuasion],
-    ) -> Persuasion:
-        return Persuasion.TRUTH
-
-    def get_belief(
-        self,
-        player_actions: list[Action],
-        player_persuasions: list[Persuasion],
-        player_beliefs: list[Belief],
-        opponent_actions: list[Action],
-        opponent_persuasions: list[Persuasion],
-    ) -> Belief:
-        return Belief.BELIEVE
-
     def get_action(
         self,
         player_actions: list[Action],
